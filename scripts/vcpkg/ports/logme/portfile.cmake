a5c8747afd8888cef2457359682bb298e89ed6b4
Christian Rauch <Rauch.Christian@gmx.de>
2026-08-19 08:29:59 +0200

    Rebase from 'release/lyrical/pangolin'

--- scripts/vcpkg/ports/logme/portfile.cmake
+++ scripts/vcpkg/ports/logme/portfile.cmake
@@ -2,8 +2,10 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO efmsoft/logme
   REF "v${VERSION}"
-  SHA512 7afb4452cc1c7a38cca0b2b951d2828f16d6fdacb456284d5896281f915bd9170f1667df443f9ce49c7f4f5dc790498215a14cc8196085ac3254378c12a924c5
-  HEAD_REF master
+  SHA512 04cedb62185460fcdfe62a163f67391e0d3877108df82589bc579604db883ef3bb068d1a9e48099609ef7fa82d3ddf65c25abf7c81f69a7bc54d40398eadcdd6
+  HEAD_REF main
+  PATCHES
+    fix-windows-static-link.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}"  "static" _logme_static_opt)
@@ -18,6 +20,8 @@ vcpkg_cmake_configure(
     -DLOGME_BUILD_EXAMPLES=OFF
     -DLOGME_BUILD_TOOLS=OFF
     -DUSE_JSONCPP=ON
+    -DUSE_ZLIB=OFF
+    -DLOGME_FMT_FORMAT=OFF
 )
 
 vcpkg_cmake_install()
@@ -25,5 +29,12 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/logme)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/Logme/Json")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+  vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/Logme/Types.h" "!defined(_LOGME_STATIC_BUILD_)" "1")
+else()
+  vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/Logme/Types.h" "!defined(_LOGME_STATIC_BUILD_)" "0")
+endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
